e39935a3f8bb9e6aeb3fca28fdc67560d189db72
Twtamaris <saurabbaral03@gmail.com>
2022-07-22 23:05:33 +0545

    Initial commit

---
+++ main1.py
@@ -0,0 +1,126 @@
+import pygame
+from pygame import mixer
+from fighter1 import Fighter
+mixer.init()
+pygame.init()
+
+#create game window
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Brawler")
+
+#set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+#define colours
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
+#define game variables
+intro_count = 3
+last_count_update = pygame.time.get_ticks()
+score = [0, 0]#player scores. [P1, P2]
+round_over = False
+ROUND_OVER_COOLDOWN = 2000
+
+#define fighter variables
+WARRIOR_SIZE = 162
+WARRIOR_SCALE = 4
+WARRIOR_OFFSET = [72, 56]
+WARRIOR_DATA = [WARRIOR_SIZE, WARRIOR_SCALE, WARRIOR_OFFSET]#list
+WIZARD_SIZE = 250
+WIZARD_SCALE = 3
+WIZARD_OFFSET = [112, 107]
+WIZARD_DATA = [WIZARD_SIZE, WIZARD_SCALE, WIZARD_OFFSET]#list
+
+#load music and sounds
+pygame.mixer.music.load("assets/audio/music.mp3")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1, 0.0, 5000)
+sword_fx = pygame.mixer.Sound("assets/audio/sword.wav")
+sword_fx.set_volume(0.5)
+magic_fx = pygame.mixer.Sound("assets/audio/magic.wav")
+magic_fx.set_volume(0.75)
+
+#load background image
+bg_image = pygame.image.load("assets/images/background/background.jpg").convert_alpha()
+
+#load spritesheets
+warrior_sheet = pygame.image.load("assets/images/warrior/Sprites/warrior.png").convert_alpha()
+wizard_sheet = pygame.image.load("assets/images/wizard/Sprites/wizard.png").convert_alpha()
+
+#load vicory image
+victory_img = pygame.image.load("assets/images/icons/victory.png").convert_alpha()
+
+#define number of steps in each animation
+WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
+WIZARD_ANIMATION_STEPS = [8, 8, 1, 8, 8, 3, 7]
+
+#define font
+count_font = pygame.font.Font("assets/fonts/turok.ttf", 80)
+score_font = pygame.font.Font("assets/fonts/turok.ttf", 30)
+
+def draw_bg():
+  scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+  screen.blit(scaled_bg, (0, 0))
+
+def draw_text(text, font, text_col, x, y):
+  img = font.render(text, True, text_col)
+  screen.blit(img, (x, y))
+
+
+  
+  
+fighter_1 = Fighter(1, 200, 310, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx)
+fighter_2 = Fighter(2, 700, 310, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS, magic_fx)
+  
+
+def draw_health_bar(health, x, y):
+  ratio = health / 100
+  pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
+  pygame.draw.rect(screen, RED, (x, y, 400, 30))
+  pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
+
+
+
+run = True
+while run:
+  clock.tick(FPS)
+  draw_bg()
+  
+  #shw playes status
+  draw_health_bar(fighter_1.health, 20, 20)
+  draw_health_bar(fighter_2.health, 580, 20)
+  draw_text("P1: " + str(score[0]), score_font, RED, 20, 60)
+  draw_text("P2: " + str(score[1]), score_font, RED, 580, 60)
+  
+  
+    
+  
+  
+  
+  
+  
+  
+  
+  #quit pygame if window is closed
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
+    # if used pressed K_ESCAPE, quit game
+    if pygame.key.get_pressed()[pygame.K_ESCAPE]:
+      run = False
+  
+  #show player Status
+  
+  #Update Display
+  pygame.display.update()
+    
+      
+pygame.quit()
+  
+  
